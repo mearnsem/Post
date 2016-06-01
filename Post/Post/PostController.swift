58df6ded16a089f7814609d199e934b5790201ac
Emily Mearns <mearnsem@gmail.com>
2016-06-01 15:47:46 -0600

    Working on the PostController

---
+++ Post/Post/PostController.swift
@@ -0,0 +1,17 @@
+//
+//  PostController.swift
+//  Post
+//
+//  Created by Emily Mearns on 6/1/16.
+//  Copyright © 2016 Emily Mearns. All rights reserved.
+//
+
+import Foundation
+
+class PostController {
+    
+    static let baseURL = NSURL(string: <#T##String#>)
+    static let endpoint = 
+    
+    
+}
