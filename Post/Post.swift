3e98b9181f2820151fd58da60b42ae10b3bb3565
Emily Mearns <mearnsem@gmail.com>
2016-06-06 14:12:54 -0600

    Initial Commit
    For the third time

--- Post/Post.swift
+++ Post/Post.swift
@@ -7,38 +7,3 @@
 //
 
 import Foundation
-
-struct Post {
-    
-    private let UsernameKey = "username"
-    private let TextKey = "text"
-    private let TimestampKey = "timestamp"
-    private let UUIDKey = "uuid"
-    
-    let username: String
-    let text: String
-    let timestamp: NSTimeInterval
-    let identifier: NSUUID
-    
-    init(username: String, text: String, identifier: NSUUID = NSUUID()) {
-        
-        self.username = username
-        self.text = text
-        self.timestamp = NSDate().timeIntervalSince1970
-        self.identifier = identifier
-    }
-    
-    init?(json: [String: AnyObject], identifier: String) {
-        
-        guard let username = json[UsernameKey] as? String,
-            let text = json[TextKey] as? String,
-            let timestamp = json[TimestampKey] as? Double,
-            let identifier = NSUUID(UUIDString: identifier) else { return nil }
-        
-        self.username = username
-        self.text = text
-        self.timestamp = NSTimeInterval(floatLiteral: timestamp)
-        self.identifier = identifier
-    }
-    
-}
