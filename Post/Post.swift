b16999d4da9a502cb36353378ab37361dc34baaf
Emily Mearns <mearnsem@gmail.com>
2016-06-06 15:13:20 -0600

    Implement Model

--- Post/Post.swift
+++ Post/Post.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+struct Post {
+    
+    private let keyUsername = "username"
+    private let keyText = "text"
+    private let keyTimestamp = "timestamp"
+    
+    let username: String
+    let text: String
+    let timestamp: NSTimeInterval
+    let identifier: NSUUID
+    
+//    var queryTimestamp: {
+//        
+//    }
+    
+    init(username: String, text: String, timestamp: NSTimeInterval = NSDate().timeIntervalSince1970, identifier: NSUUID = NSUUID()) {
+        self.username = username
+        self.text = text
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+    
+    init?(dictionary: [String: AnyObject], identifier: String) {
+        guard let username = dictionary[keyUsername] as? String,
+            text = dictionary[keyText] as? String,
+            timestamp = dictionary[keyTimestamp] as? Double,
+            identifier = NSUUID(UUIDString: identifier) else {return nil}
+        
+        self.username = username
+        self.text = text
+        self.timestamp = NSTimeInterval(floatLiteral: timestamp)
+        self.identifier = identifier
+    }
+}
